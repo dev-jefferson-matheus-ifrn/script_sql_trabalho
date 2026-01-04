0ccb1fc9d329270e5057b2d63eeffdbcc0209837
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2026-01-04 13:43:40 -0300

    Criação do script sql

---
+++ clinica_medica.sql
@@ -0,0 +1,10 @@
+/*
+SCRIPT: clinica_medica
+OBJETIVO: Criar banco de dados, tabelas, inserir dados, e realizar consultas
+SGBD: MySQL 8.x
+AUTORES: Jefferson Matheus Ferreira de Lima, Ryan Kauan Medeiros de Souza, Jose Viton dos Santos
+*/
+
+CREATE DATABASE IF NOT EXISTS clinica
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
